7a21783a5a67224be21deab3cb136e6063dc8bdf
[Marek] <marekkabala1@gmail.com>
2023-05-29 15:31:47 +0100

    Rest Api implemented with conection to MongoDB and types for data

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express"
+import * as dotenv from "dotenv"
+import colors from 'colors'
+import connectDB from "./config/db"
+import { routes } from './routes/routes'
+
+dotenv.config()
+
+colors.enable();
+colors.enable();
+
+const port = process.env.PORT || 5001
+const app: Express = express()
+
+connectDB()
+
+app.use(express.json())
+
+app.use('/nutrition', routes)
+
+app.listen(5000, () => {
+  console.log(`Application started on http://localhost:${port}/nutrition`.underline.cyan)
+})
